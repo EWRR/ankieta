b09cd8e9fa50d78cfca5d8c1119380ab4ac52f0d
Wojtek <rybinski05@gmail.com>
2018-05-04 14:41:12 +0200

    Removed config from git

--- app/config.js
+++
@@ -1,8 +0,0 @@
-const config =  {
-	secret: 'pepowojtekemil',
-	token: 'EAAWsfNTXPFYBAKGCaWbZCAWLGQ0TTgxI4BQrn52kfPkTnTaaZCZCA3BPZCAp8Rdj6nZAZAHbgo2H4Gj9u2HL9o2XjpVPBRE4gZBUEgqn5Dpst6Cxl3R4Wyeeeki1dIjoyTyHZAWmRlD6VZA8A7VJYIgCTzZCYheAEu8psuFK8DsdIPDAZDZD'
-};
-
-module.exports = {
-	config
-};
